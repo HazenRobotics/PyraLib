b50986a30456aa1248779f7ef36c7692058bacbf
Corban Thompson <corbant25@gmail.com>
2022-07-21 08:10:32 -0700

    Initial Commit

---
+++ core/src/main/java/com/hazenrobotics/pyralib/drive/Test.java
@@ -0,0 +1,34 @@
+package com.hazenrobotics.pyralib.drive;
+
+import com.hazenrobotics.pyralib.util.UtilTest;
+
+/**
+ * This is a test class!
+ */
+public class Test<T extends UtilTest> {
+
+    private String variable;
+
+    /**
+     * This method does absolutely nothing!
+     */
+    public void thingy() {
+
+    }
+
+    /**
+     * Gets the variable named variable
+     * @return the string variable
+     */
+    public String getVariable() {
+        return variable;
+    }
+
+    /**
+     * sets the variable named variable
+     * @param newVar The new variable to set variable to
+     */
+    public void setVariable(String newVar) {
+        variable = newVar;
+    }
+}
---
+++ core/src/main/java/com/hazenrobotics/pyralib/util/UtilTest.java
@@ -0,0 +1,7 @@
+package com.hazenrobotics.pyralib.util;
+
+/**
+ * This is a test utility class
+ */
+public class UtilTest {
+}
